4b3dc6ab6e1c47f2826c602aa8e22862c2f31da5
Archita <122664337+architajain05@users.noreply.github.com>
2023-02-11 16:42:50 +0530

    Create to calculate area of circle.c

---
+++ to calculate area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ 	float radius,area;
+	printf("Enter the radius of circle:");
+	scanf("%f",&radius);
+	area=3.14*radius*radius;
+	printf("Area=%.2f",area);
+	return 0;
+}
+
+
